68309d9184657e6b10abd9c7ec522a73d8a2b9f0
Xabriel J. Collazo Mojica <xcollazo@wikimedia.org>
2022-11-09 09:51:05 -0500

    Revert "Default spark sessions to Spark3. (#34)"
    This reverts commit 4816e6bb15f6882893555d90422a512abb4342a4 as it is
    blocking testing #36. We will merge back in later.

--- wmfdata/spark.py
+++ wmfdata/spark.py
@@ -15,13 +15,7 @@ from wmfdata.utils import (
 """
 Will be used for findspark.init().
 """
-SPARK_HOME = os.environ.get("SPARK_HOME", "/usr/lib/spark3")
-if SPARK_HOME == "/usr/lib/spark2":
-    warnings.warn(
-        "Spark2 has been deprecated. Please upgrade your jobs to Spark3. "
-        "See https://phabricator.wikimedia.org/T318367 for details.",
-        category=FutureWarning
-    )
+SPARK_HOME = os.environ.get("SPARK_HOME", "/usr/lib/spark2")
 
 """
 Predefined spark sessions and configs for use with the get_session and run functions.
